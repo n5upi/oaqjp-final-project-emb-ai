b47fd5ed5efc627279094ff316499b8d5945a34c
n5upi <dallasats@hotmail.com>
2024-07-26 23:51:21 -0400

    Adding temporary changes to Github

--- server.py
+++ server.py
@@ -9,11 +9,22 @@ def emot_detector():
     text_to_analyze = request.args.get('textToAnalyze')
 
     # Pass the text to the emotion_detector function and store the response
-    return response = emotion_detector(text_to_analyze)
+    response = emotion_detector(text_to_analyze)
 
     # Extract the label and score from the response
-    #label = response['label']
-    #score = response['score']
+    disgust = "'disgust': {}, ".format(response['disgust'])
+    fear =  "'fear': {}, ".format(response['fear'])
+    joy = "'joy': {}, ".format(response['joy'])
+    sadness = " and 'sadness': {}. ".format(response['sadness'])
+    dominant = "The dominant emotion is {}.".format(response['dominant_emotion'])
 
-    # Return a formatted string with the sentiment label and score
-    #return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+    # Return a formatted string
+    return "For the given statement, the system response is 'anger': {}, ".format(response['anger']) + disgust + fear + joy + sadness + dominant
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    # This functions executes the flask app and deploys it on localhost:5000
+    app.run(host="0.0.0.0", port=5000)
